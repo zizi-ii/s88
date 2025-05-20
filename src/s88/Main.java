5755884f88fae61bb6f2b5f0e79bcd1aaa5e65ba
Lee, Jiwoo <zizii23027@gmail.com>
2025-05-20 18:18:11 +0900

    five

--- src/s88/Main.java
+++ src/s88/Main.java
@@ -8,5 +8,7 @@ public class Main {
 		System.out.println(Hello.sum(10)); 
 		System.out.println(Hello.sum(100));
 		System.out.println(Hello.sum(1000));
+		System.out.println(Sansu.max(new int[] {8, 5, 6, 9, 4, 1, 7, 10, 3, 2, 8})); 
+		System.out.println(Sansu.max(new int[] {-2, 5, 6, 9, -6, 1, 7, 2, 8, 10}));
 	}
 }
